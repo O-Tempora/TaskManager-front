77eea8bc1e9c4fdb6d3a07aafcfa09852b27b977
Валентин Воронин <valentraven@gmail.com>
2023-05-25 01:40:22 +0300

    feat: update task sidebar (except comments and assigning)

--- src/routes/workspace-[slug]/+layout.js
+++ src/routes/workspace-[slug]/+layout.js
@@ -1,8 +1,9 @@
 import { workspace } from "../workspace";
+import { jwt } from "../jwt";
+import { statuses } from "../statuses";
 
 export async function load ({ fetch, params, url }){
     const usr = url.searchParams.get('usr');
-    console.log(usr);
     if (usr === null || ""){
         return;
     }
@@ -12,8 +13,30 @@ export async function load ({ fetch, params, url }){
             'Authorization': "Bearer " + usr,
         }
     });
-    let data = await res.json();
+    const res_admin = await fetch(`http://localhost:5192/person/isAdmin-${params.slug}`, {
+        method: "get",
+        headers:{
+            'Authorization': "Bearer " + usr,
+        }
+    });
+    const res_statuses = await fetch(`http://localhost:5192/status`, {
+        method: "get",
+        headers:{
+            'Authorization': "Bearer " + usr,
+        }
+    });
 
-    workspace.set(data);
-    return {data: data}
+    let data = await res.json();
+    let isAdmin = await res_admin.json();
+    let stat = await res_statuses.json();
+
+    workspace.set({data, isAdmin});
+    jwt.set(usr);
+    statuses.set(stat);
+
+    return {
+        data: data,
+        isAdmin: isAdmin,
+        statuses: stat
+    }
 }
--- src/routes/workspace-[slug]/+page.svelte
+++ src/routes/workspace-[slug]/+page.svelte
@@ -4,20 +4,17 @@
     import Group from "../../lib/Group.svelte"
     import { workspace } from "../workspace";
     import { task } from "../task";
+    import { statuses } from "../statuses";
     import 'iconify-icon';
     import { fly } from 'svelte/transition';
 
     export let data;
-    workspace.subscribe((value) => {
-        data = value;
-    });
     
     let openTask = false;
     let showTask = async (event) => {
         openTask = true;
         let t = await getTask(event.detail.id)
         task.set(t);
-        console.log("Shown task:", $task);
     }
     let getTask = async (id) =>{
         try{
@@ -28,7 +25,31 @@
                 },
             });
             const data = await resp.json();
+            data.startAt = data.startAt.slice(0, 10)
+            data.finishAt = data.finishAt.slice(0, 10)
             return data;
+        } catch(e){
+            console.log(e)
+        }
+    }
+    let saveTask = async () => {
+        let t = {...$task}; //copy object
+        t.startAt = new Date(parseInt(t.startAt.slice(0, 4)), parseInt(t.startAt.slice(5, 7)) - 1, parseInt(t.startAt.slice(8)));
+        t.finishAt = new Date(parseInt(t.finishAt.slice(0, 4)), parseInt(t.finishAt.slice(5, 7)) - 1, parseInt(t.finishAt.slice(8)));
+        try{
+            await axios.put(
+                `http://localhost:5192/task/${t.id}`,
+                t
+            )
+            let group = $workspace.data.groups.findIndex(v => v.id === $task.groupId);
+            let tsk = $workspace.data.groups[group].tasks.findIndex(v => v.id === $task.id);
+            $workspace.data.groups[group].tasks[tsk] = {
+                id: $task.id,
+                createdAt: $workspace.data.groups[group].tasks[tsk].createdAt,
+                description: $task.description,
+                status: $task.status,
+                persons: $workspace.data.groups[group].tasks[tsk].persons
+            }
         } catch(e){
             console.log(e)
         }
@@ -45,7 +66,7 @@
                 }
             );
             const gr = await res.data;
-            $workspace.groups = [...$workspace.groups, {name: gr.name,color: gr.color, id: gr.id,}]
+            $workspace.data.groups = [...$workspace.data.groups, {name: gr.name,color: gr.color, id: gr.id,}]
         } catch(e){
             console.log(e);
         }
@@ -53,16 +74,81 @@
 </script>
 
 {#if openTask && $task != null}
-<div class="fixed top-0 right-0 bg-zinc-800 h-full w-[584px] flex flex-col" transition:fly={{x: '100%'}}>
-    <div class="w-full mx-4 my-4">
-        <button class="text-white text-4xl hover:bg-zinc-700 rounded-lg p-1" on:click={() => openTask = false}>
-            <iconify-icon icon="material-symbols:close"/>
-        </button>
+    <div class="fixed top-0 right-0 bg-zinc-800 h-full w-[584px] flex flex-col p-4" transition:fly={{x: '100%'}}>
+        <div class="w-full flex flex-row justify-between">
+            <button class="text-white text-3xl hover:bg-zinc-700 rounded-lg p-2 flex flex-row items-center" on:click={() => saveTask()}>
+                <iconify-icon icon="fluent-mdl2:save"/>
+                <p class="ml-2">Save</p>
+            </button>
+            <button class="text-white text-4xl hover:bg-zinc-700 rounded-lg p-1" on:click={() => openTask = false}>
+                <iconify-icon icon="material-symbols:close"/>
+            </button>
+        </div>
+        <div class="px-4 w-full">
+            <div class="text-gray-100 text-right text-lg font-bold">
+                Created: {$task.createdAt.slice(0,10)}
+            </div>
+            {#if $workspace.isAdmin}
+                <div contenteditable="true" class="text-white text-2xl font-serif my-2 p-2 rounded-lg border-2 border-stone-500" bind:textContent={$task.description}></div>
+            {:else}
+                <div contenteditable="false" class="text-white text-2xl font-serif my-2 p-2 rounded-lg border-2 border-stone-500">
+                    {$task.description}
+                </div>
+            {/if}
+            <div class="flex flex-col w-full justify-between">
+                {#if $workspace.isAdmin}
+                    <div class="flex flex-row mt-2">
+                        <p class="text-xl text-green-400 font-bold py-2">Start At:</p>
+                        <input type="date" bind:value={$task.startAt} class="text-lg bg-zinc-700 text-white p-1 rounded-lg ml-6"/>
+                    </div>
+                    <div class="flex flex-row mt-2">
+                        <p class="text-xl text-[#ff6e40] font-bold py-2">Finish At:</p>
+                        <input type="date" bind:value={$task.finishAt} class="text-lg bg-zinc-700 text-white p-1 rounded-lg ml-4"/>
+                    </div>
+                {:else}
+                    <div class="flex flex-row mt-2">
+                        <p class="text-xl text-green-400 font-bold py-2">Start At:</p>
+                        <p class="text-lg bg-zinc-700 text-white p-1 rounded-lg ml-6">{$task.startAt}</p>
+                    </div>
+                    <div class="flex flex-row mt-2">
+                        <p class="text-xl text-[#ff6e40] font-bold py-2">Finish At:</p>
+                        <p class="text-lg bg-zinc-700 text-white p-1 rounded-lg ml-4">{$task.finishAt}</p>
+                    </div>
+                {/if}
+            </div>
+            <div class="flex flex-row w-full justify-start mt-2">
+                <p class="text-xl text-white font-bold py-2">Status:</p>
+                {#if $workspace.isAdmin}
+                    <select bind:value={$task.status} class="ml-4 rounded-md bg-zinc-700 text-white px-2 text-lg">
+                        {#each $statuses as status}
+                            <option value={status.name}>{status.name}</option>
+                        {/each}
+                    </select>
+                {:else}
+                     <p class="ml-4 rounded-md bg-zinc-700 text-white px-2 text-lg">{$task.status}</p>
+                {/if}
+            </div>
+            <div>
+                <p class="mt-4 text-white font-bold italic text-3xl">Assigned:</p>
+                <ul class="flex flex-col">
+                    {#each $workspace.data.groups
+                        .filter(v => v.id === $task.groupId)[0].tasks
+                        .filter(t => t.id === $task.id)[0].persons as person}
+                        <li>
+                            <div class="flex flex-row justify-between p-1 items-center">
+                                {#if person.role === "Admin"}
+                                    <p class="text-sky-400 text-xl">{person.name}</p>
+                                    <iconify-icon icon="fluent:dismiss-circle-32-regular" class="text-3xl text-red-500 hover:bg-zinc-700 rounded-lg p-1"/>
+                                {:else}    
+                                    <p class="text-amber-200 text-lg">{person.name}</p>
+                                {/if}
+                            </div>
+                        </li>
+                    {/each}
+                </ul>
+            </div>
+        </div>
     </div>
-    <div contenteditable="true" class="mx-8 text-white text-2xl font-serif p-2">
-        {$task.description}
-    </div>
-</div>
 {/if}
 
 <section class="flex flex-row content-center items-start h-full w-full">
@@ -72,5 +158,5 @@
             <p class="text-xl">Add Group</p>
         </button>
     </nav>
-    <Group bind:group={$workspace.groups} bind:ws_id={$workspace.id} on:opentask={showTask}/>
+    <Group bind:group={$workspace.data.groups} bind:ws_id={$workspace.data.id} on:opentask={showTask}/>
 </section>
